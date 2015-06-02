fed7a7d84affb69fb9ba938972e3908eb02490a5
silvernode <silvernode@gmail.com>
2015-06-02 03:31:37 -0500

    Put limit on password length

--- functions.sh
+++ functions.sh
@@ -13,12 +13,20 @@ shaStatic(){
 
 shaWithNum(){
   #hashes date with SHA
-  echo
-  echo "$1 character password generated using SHA256 with date"
-  echo "***************************************************"
-  date +%s | sha256sum | base64 | head -c $1 ; echo
-  echo "***************************************************"
+  if [ $1 -ge 255 ];then
+    echo
+    echo "Error : $0 : Please choose a number lower than 255"
+    echo
+
+  else
+    echo
+    echo "$1 character password generated using SHA256 with date"
+    echo "***************************************************"
+    date +%s | sha256sum | base64 | head -c $1 ; echo
+    echo "***************************************************"
+  fi
 }
+
 
 leftHand(){
   echo
@@ -29,9 +37,16 @@ leftHand(){
 }
 
 leftHandNum(){
-  echo
-  echo "$1 character password easy to type with left hand"
-  echo "**************************************************"
-  </dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c$1; echo ""
-  echo "**************************************************"
+  if [ $1 -ge 255 ];then
+    echo
+    echo "Error : $0 : Please choose a number lower than 255"
+    echo
+
+  else
+    echo
+    echo "$1 character password easy to type with left hand"
+    echo "**************************************************"
+    </dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c$1; echo ""
+    echo "**************************************************"
+  fi
 }
